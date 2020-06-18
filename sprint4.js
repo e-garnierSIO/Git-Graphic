2d6b153b93ea3ab47fcb49f2b01ba72e21990d1e
jesterzon <julien.esterzon@gmail.com>
2020-06-18 10:42:02 +0200

    Update sprint4.js

--- sprint4.js
+++ sprint4.js
@@ -10,6 +10,7 @@ integration.commit("start");
 // Branches FONCTIONNALITE et TACHES
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_refonte_moteur_sensibilisation_relance");
 var tache3 = creerBrancheDeTache(fonctionnalite1, "3577-suppression-pop-in-existante-sur-verrouillage-smartphone");
+var tache4 = creerBrancheDeTache(fonctionnalite1, "a_3560-Affichage-picto-sensi-verrouillage-telephone");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_evolution_notifications");
 var tache2 = creerBrancheDeTache(fonctionnalite2, "2235_mise_a_jour_centre_notifications");
 var fonctionnalite3 = creerBrancheDeFonctionnalite("f_inbenta");
@@ -21,8 +22,9 @@ var fonctionnalite4 = creerBrancheDeFonctionnalite("f_refacto_centre_notif_moteu
 //var brancheSprint = creerBrancheDeSprint("twenty_r01_s04");
 
 // Branche MERGE
+fonctionnalite1.merge(tache4);
+fonctionnalite4.merge(tache2);
 fonctionnalite4.merge(fonctionnalite1);
-fonctionnalite4.merge(fonctionnalite2);
 // MERGES --------------------
 
 
