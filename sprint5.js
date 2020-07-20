c52eef477c8eb0875964b63f55149279ef001af5
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-07-20 09:21:47 +0200

    Create sprint5.js

---
+++ sprint5.js
@@ -0,0 +1,22 @@
+// On instancie le graph dans le conteneur.
+var sprint5 = creerGitGraph("graph-container-5");
+
+// Branche MASTER
+var master = creerBrancheMaster(sprint5);
+
+var integration = master.branch("integration_app_20_2");
+integration.commit("start");
+
+// Branches FONCTIONNALITE et TACHES
+
+
+// Branche SPRINT
+
+
+// Branche MERGE
+
+
+// MERGES --------------------
+
+
+// FUSION aprés DEMO
