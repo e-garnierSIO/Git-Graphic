47c08250464fb38c6f6d50480198d24ce2b2d206
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-08-31 11:45:25 +0200

    Update sprint7.js

--- sprint7.js
+++ sprint7.js
@@ -17,6 +17,7 @@ var fonctionnalite1 = creerBrancheDeFonctionnalite("f_ibenta");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "4300-inbenta-mise-a-jour-url-prod");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_IARD");
 var tache2 = creerBrancheDeTache(fonctionnalite2, "4329-iard-changement-url-environnement-pre-prod");
+var tache3 = creerBrancheDeTache(fonctionnalite2, "3360-4169-iard-changement-libelles");
 var fonctionnalite3 = creerBrancheDeFonctionnalite("f_smartblette");
 
 // Branche SPRINT
