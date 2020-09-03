8d88e5631b57bc902a6d915b125fa8e531118c3b
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-09-03 10:14:04 +0200

    Update sprint7.js

--- sprint7.js
+++ sprint7.js
@@ -18,6 +18,7 @@ var tache1 = creerBrancheDeTache(fonctionnalite1, "4300-inbenta-mise-a-jour-url-
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_IARD");
 var tache2 = creerBrancheDeTache(fonctionnalite2, "4329-iard-changement-url-environnement-pre-prod");
 var tache3 = creerBrancheDeTache(fonctionnalite2, "3360-4169-iard-changement-libelles");
+var tache4 = creerBrancheDeTache(fonctionnalite2, "4577-SB-IARD");
 var fonctionnalite3 = creerBrancheDeFonctionnalite("f_smartblette");
 var tache4 = creerBrancheDeTache(integration, "4167-localstorage-integration-maintien-du-profil-avec-wk-webview");
 
