dd4ecc0d0761fe648790e6c610a487b4748f8010
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-09-16 12:07:11 +0200

    Create sprint8.js

---
+++ sprint8.js
@@ -0,0 +1,26 @@
+// On instancie le graph dans le conteneur.
+var sprint8 = creerGitGraph("graph-container-8");
+
+
+// Branche MASTER
+var master = creerBrancheMaster(sprint8);
+
+var integration = master.branch("integration_systemteam_r20_4");
+integration.commit("start");
+
+
+
+// Branches FONCTIONNALITE et TACHES
+
+
+// Branche SPRINT
+
+
+// Branche MERGE
+
+
+// MERGES --------------------
+
+
+
+// FUSION aprés DEMO
