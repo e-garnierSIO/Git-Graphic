c9826df0cc6e575e2205f35773ae5d5c092b8986
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-09-16 14:26:50 +0200

    Update sprint8.js

--- sprint8.js
+++ sprint8.js
@@ -8,9 +8,10 @@ var master = creerBrancheMaster(sprint8);
 var integration = master.branch("integration_systemteam_r20_4");
 integration.commit("start");
 
-
-
 // Branches FONCTIONNALITE et TACHES
+var tache1 = creerBrancheDeTache(integration, "a_4674-iard-affichage-la-vignette-promotionnelle-pour-un-client-avec-une-assurance-hors-perimetrie");
+
+
 
 
 // Branche SPRINT
