48e2a732c8d8537e3f60a347df0541961557b8dd
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-09-18 10:59:09 +0200

    Update sprint8.js

--- sprint8.js
+++ sprint8.js
@@ -10,7 +10,8 @@ integration.commit("start");
 
 // Branches FONCTIONNALITE et TACHES
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_4173_homogeneisation_elements_stockage");
-
+var tache1 = creerBrancheDeTache(integration, "correction_connection_tablette_20_3");
+var tache2 = creerBrancheDeTache(integration, "a_4674-iard-affichage-la-vignette-promotionnelle-pour-un-client-avec-une-assurance-hors-perimetrie");
 
 
 
@@ -20,6 +21,7 @@ var fonctionnalite1 = creerBrancheDeFonctionnalite("f_4173_homogeneisation_eleme
 
 
 // Branche MERGE
+creerMerge(integration, tache1);
 
 
 // MERGES --------------------
