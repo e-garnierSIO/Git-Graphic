2f27a78f1108d159cfba24b3a02333f2a80fcf93
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-09-22 09:43:25 +0200

    Update sprint8.js

--- sprint8.js
+++ sprint8.js
@@ -13,11 +13,9 @@ var tache1 = creerBrancheDeTache(integration, "correction_connection_tablette_20
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_4719_outillage_sdkta");
 var tache3 = creerBrancheDeTache(integration, "344-q4q-4702-certicode-crash-de-l-application-a-l-enrolement");
 var tache4 = creerBrancheDeTache(integration, "a_4748-mode-invite-profil-sauvegarde-a-tord-en-mode-invite");
+var tache2 = creerBrancheDeTache(integration, "a_4674-iard-affichage-la-vignette-promotionnelle-pour-un-client-avec-une-assurance-hors-perimetrie");
 var tache5 = creerBrancheDeTache(integration, "a_4647_page_blanche");
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_4173_homogeneisation_elements_stockage");
-var tache2 = creerBrancheDeTache(integration, "a_4674-iard-affichage-la-vignette-promotionnelle-pour-un-client-avec-une-assurance-hors-perimetrie");
-
-
 
 
 
@@ -29,6 +27,8 @@ var tache2 = creerBrancheDeTache(integration, "a_4674-iard-affichage-la-vignette
 creerMerge(integration, tache1);
 creerMerge(integration, fonctionnalite2);
 creerMerge(integration, tache3);
+creerMerge(integration, tache4);
+creerMerge(integration, tache2);
 
 
 // MERGES --------------------
