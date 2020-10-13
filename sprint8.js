5a56e7ed699a2566055aad7037890741b95dc767
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-10-13 11:40:52 +0200

    Update sprint8.js

--- sprint8.js
+++ sprint8.js
@@ -70,8 +70,3 @@ var integration5 = creerBrancheDeMerge(integration4, "integration_systemteam_r20
 creerMerge(integration5, tache14);
 creerMerge(integration5, tache15);
 
-// Branche d'integration (version 6)
-var integration6 = creerBrancheDeMerge(integration5, "integration_systemteam_r20_4 (?)");
-
-// MERGES (version 6)--------------------
-
