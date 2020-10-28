f97cdaf945ba3caf004be5c63273c196c04c7087
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-10-28 08:12:02 +0100

    Update sprint9.js

--- sprint9.js
+++ sprint9.js
@@ -12,7 +12,7 @@ integration.commit("start");
 // Branches FONCTIONNALITE et TACHES
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_vulnerabilites-strandhogg");
 var tache1 = creerBrancheDeTache(fonctionnalite2, "4555-vulnerabilites-strandhogg");
-var tache2 = creerBrancheDeTache(integration, "a_4511-probleme-au-clic-sur-le-bouton-annuler");
+var anomalie1 = creerBrancheAnomalie(integration, "a_4511-probleme-au-clic-sur-le-bouton-annuler");
 var fonctionnalite3 = creerBrancheDeFonctionnalite("f_wkwebview");
 var tache3 = creerBrancheDeTache(fonctionnalite3, "5007-integration-wkwebview");
 var fonctionnalite4 = creerBrancheDeFonctionnalite("f_4173_homogeneisation_elements_stockage");
@@ -22,9 +22,9 @@ var tache6 = creerBrancheDeTache(fonctionnalite6, "poc_4953-iard-pdf-iFrame");
 var tache4 = creerBrancheDeTache(fonctionnalite6,"5089-IARD-changement-du-service-bancaire");
 var fonctionnalite7 = creerBrancheDeFonctionnalite("f_evolution_notifications");
 var tache5 = creerBrancheDeTache(fonctionnalite7, "5081-notification-app-en-time-out");
-var tache7 = creerBrancheDeTache(integration, "a_4520-ios-click-to-call");
-var tache8 = creerBrancheDeTache(integration, "5179-ios-acces-comptes-autre-personne");
-var tache9 = creerBrancheDeTache(integration, "a_5035_refacto");
+var anomalie2 = creerBrancheAnomalie(integration, "a_4520-ios-click-to-call");
+var anomalie3 = creerBrancheAnomalie(integration, "5179-ios-acces-comptes-autre-personne");
+var anomalie4 = creerBrancheAnomalie(integration, "a_5035_refacto");
 
 
 // Branche SPRINT
@@ -35,7 +35,7 @@ var tache9 = creerBrancheDeTache(integration, "a_5035_refacto");
 
 // MERGES --------------------
 fonctionnalite2.merge(tache1);
-creerMerge(integration, tache2);
+creerMerge(integration, anomalie1);
 fonctionnalite3.merge(tache3);
 creerMerge(integration, fonctionnalite3);
 integration.commit("V20.05.00.900 à 903"); // tag
@@ -43,6 +43,6 @@ fonctionnalite6.merge(tache6);
 creerMerge(integration, fonctionnalite5);
 fonctionnalite6.merge(tache4);
 fonctionnalite7.merge(tache5);
-fonctionnalite7.merge(tache9);
+fonctionnalite7.merge(anomalie4);
 
 // FUSION aprés DEMO
