cca4dbde11fe06244fa62e8c09b0282d21eff9c4
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-11-04 14:01:10 +0100

    Create sprint10.js

---
+++ sprint10.js
@@ -0,0 +1,28 @@
+// On instancie le graph dans le conteneur.
+var sprint10 = creerGitGraph("graph-container-10");
+
+// Branche MASTER
+var master = creerBrancheMaster(sprint10);
+
+var integration = master.branch("integration_systemteam_R20_5");
+integration.commit("start");
+
+
+
+// Branches FONCTIONNALITE et TACHES
+
+
+
+
+// Branche SPRINT
+//var brancheSprint = creerBrancheDeSprint("twenty_r05_s02");
+
+
+// Branche MERGE
+
+
+// MERGES --------------------
+
+
+
+// FUSION aprés DEMO
