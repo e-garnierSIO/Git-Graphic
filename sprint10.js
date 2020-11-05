57d5ad0e6db9a04db3cc2d23519422783a2d8b0d
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-11-05 11:09:52 +0100

    Update sprint10.js

--- sprint10.js
+++ sprint10.js
@@ -11,7 +11,7 @@ integration.commit("start");
 
 // Branches FONCTIONNALITE et TACHES
 var anomalie1 = creerBrancheAnomalie(integration, "a_4520-ios-click-to-call");
-var fonctionnalite1 = creerBrancheDeFonctionnalite(integration, "f_localstorage");
+var fonctionnalite1 = creerBrancheDeFonctionnalite("f_localstorage");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "t_5246_local_storage_amelioration");
 
 
