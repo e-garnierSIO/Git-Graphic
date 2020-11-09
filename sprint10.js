f970edb35d1be491c2052fd4c1738a4d8bf6f5f5
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-11-09 16:50:42 +0100

    Update sprint10.js

--- sprint10.js
+++ sprint10.js
@@ -15,6 +15,9 @@ var fonctionnalite1 = creerBrancheDeFonctionnalite("f_localstorage");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "t_5246_local_storage_amelioration");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_IARD");
 var anomalie2 = creerBrancheAnomalie(fonctionnalite2, "a_5361_redirection_iard");
+var fonctionnalite3 = creerBrancheDeFonctionnalite("f_evolution_notifications");
+var tache2 = creerBrancheDeTache(fonctionnalite3, "t_5272_fluidifier_ouvertures_notifications");
+var tache3 = creerBrancheDeTache(integration, "t_5174_clean_sonar");
 
 
 
