150846c9505e0a897bfff4c4f1b7b16a261920e5
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-11-18 12:06:27 +0100

    Update sprint11.js

--- sprint11.js
+++ sprint11.js
@@ -4,24 +4,18 @@ var sprint11 = creerGitGraph("graph-container-11");
 // Branche MASTER
 var master = creerBrancheMaster(sprint11);
 
+// Branche d'integration (version 1)
 var integration = master.branch("integration_systemteam_R20_5");
 integration.commit("start");
 
 
-
-// Branches FONCTIONNALITE et TACHES
+// Branches (version 1)
 var anomalie1 = creerBrancheAnomalie(integration, "a_4520-ios-click-to-call");
 
 
 
-// Branche SPRINT
+// Branche d'integration (version 2)
+//var integration2 = creerBrancheDeMerge(integration, "integration_systemteam_R20_5 (v9xx et v9xx)");
 
+// MERGES (version 2)--------------------
 
-// Branche MERGE
-
-
-// MERGES --------------------
-
-
-
-// FUSION aprés DEMO
