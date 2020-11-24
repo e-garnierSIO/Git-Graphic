c119d474ce377e7f739104ef9c134c7b77e5fef0
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-11-24 16:46:04 +0100

    Update sprint11.js

--- sprint11.js
+++ sprint11.js
@@ -9,14 +9,18 @@ var integration = master.branch("integration_systemteam_R20_5");
 integration.commit("start");
 
 
-// Branches (version 1)
-var anomalie1 = creerBrancheAnomalie(integration, "a_4520-ios-click-to-call (+ 5423, 5425, 5543");
+// Branches
+var anomalie1 = creerBrancheAnomalie(integration, "a_4520-ios-click-to-call (+ 5423, 5425, 5543)");
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_multiprofil_avec_nouveau_localstorage");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "398-t_4497_integration_multiprofil_avec_nouveau_localstorage");
 var tache2 = creerBrancheDeTache(integration, "t-5512-inbenta-evolution-du-bouton-aide");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_5336_localstorage_tablette");
 var fonctionnalite3 = creerBrancheDeFonctionnalite("5429_poc_logs_c+");
 
+
+// MERGES (version 1)
+creerMerge(integration, anomalie1);
+creerMerge(integration, tache2);
 
 
 // Branche d'integration (version 2)
