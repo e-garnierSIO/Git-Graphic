8c7f26ec3f98a54d0330dd7f055c4a079dcbab56
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2020-12-04 09:10:59 +0100

    Update sprint11.js

--- sprint11.js
+++ sprint11.js
@@ -13,9 +13,9 @@ integration.commit("start");
 var anomalie1 = creerBrancheAnomalie(integration, "a_4520-ios-click-to-call (+ 5423, 5425, 5543)");
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_multiprofil_avec_nouveau_localstorage");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "398-t_4497_integration_multiprofil_avec_nouveau_localstorage");
+var tache4 = creerBrancheDeTache(tache1, "4497_5336");
 var tache2 = creerBrancheDeTache(integration, "t-5512-inbenta-evolution-du-bouton-aide (= 5517)");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_5336_localstorage_tablette");
-var tache4 = creerBrancheDeTache(fonctionnalite2, "4497_5336");
 var fonctionnalite3 = creerBrancheDeFonctionnalite("5429_poc_logs_c+");
 var anomalie2 = creerBrancheAnomalie(integration, "a_5526-tag-fingerprint");
 var anomalie3 = creerBrancheAnomalie(integration, "a_5601-loader-infini-a-la-connexion");
@@ -31,7 +31,7 @@ integration.commit("V20.05.00.914, 915 et 916"); // tag
 creerMerge(integration, anomalie3);
 integration.commit("V20.05.00.917 et 918"); // tag
 creerMerge(integration, anomalie4);
-tache4.merge(tache1);
+tache4.merge(fonctionnalite2);
 
 
 
