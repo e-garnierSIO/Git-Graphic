55674a2e4af98374e04551091c1dc0931cf57f36
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-01-07 15:02:14 +0100

    Update sprint13.js

--- sprint13.js
+++ sprint13.js
@@ -13,6 +13,7 @@ integration.commit("start");
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_simplification_notifs");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f-LBPF");
 var fonctionnalite3 = creerBrancheDeFonctionnalite("f-IARD");
+var tache1 = creerBrancheDeTache(fonctionnalite3, "t_5911-iard-table-des-id-produits");
 var fonctionnalite4 = creerBrancheDeFonctionnalite("f-DSP2");
 
 
