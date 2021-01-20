8dbe760b6b726866c6e48d51f98848bcbc2de865
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-01-20 11:01:41 +0100

    Update sprint13.js

--- sprint13.js
+++ sprint13.js
@@ -17,12 +17,13 @@ var tache2 = creerBrancheDeTache(fonctionnalite2, "t_5739-lbpf-stripes-menu-gere
 var tache6 = creerBrancheDeTache(fonctionnalite2, "t_5859-lbpf-mes-demandes-en-cours");
 var fonctionnalite3 = creerBrancheDeFonctionnalite("f-IARD");
 var tache3 = creerBrancheDeTache(fonctionnalite3, "t_5911-iard-table-des-id-produits");
+var tache7 = creerBrancheDeTache(fonctionnalite3, "t_5922-iard-integration-alignement");
 var fonctionnalite4 = creerBrancheDeFonctionnalite("f-DSP2");
 var tache4 = creerBrancheDeTache(fonctionnalite4, "t_auth_forte_moins_90_jours (5880)");
 var tache5 = creerBrancheDeTache(fonctionnalite4, "t_5898_auth_forte_plus_90_jours");
 var anomalie1 = creerBrancheAnomalie(integration, "a_6464-photo-accueil");
 var fonctionnalite5 = creerBrancheDeFonctionnalite("f_tu_chrome_headless");
-var tache6 = creerBrancheDeTache(fonctionnalite2, "t_5922-iard-integration-alignement");
+
 
 
 
