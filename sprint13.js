9fb635a471f3c04e4e05e6f5812c0d764bb46d14
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-01-22 16:09:44 +0100

    Update sprint13.js

--- sprint13.js
+++ sprint13.js
@@ -9,15 +9,15 @@ integration.commit("start");
 
 // Branches FONCTIONNALITE et TACHES
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_simplification_notifs");
-var fonctionnalite2 = creerBrancheDeFonctionnalite("f-LBPF");
-var tache1 = creerBrancheDeTache(fonctionnalite2, "t_5824-stripes-lbpf");
-var tache2 = creerBrancheDeTache(fonctionnalite2, "t_5739-lbpf-stripes-menu-gerer");
+var fonctionnalite2 = creerBrancheDeFonctionnalite("f_LBPF");
+var tache1 = creerBrancheDeTache(fonctionnalite2, "t_5824-stripes-lbpf (t_5824_BIS)");
+var tache2 = creerBrancheDeTache(fonctionnalite2, "t_5739-lbpf-stripes-menu-gerer (t_5739_BIS)");
 var tache6 = creerBrancheDeTache(fonctionnalite2, "t_5859-lbpf-mes-demandes-en-cours");
 var tache8 = creerBrancheDeTache(fonctionnalite2, "t_5850-lbpf-en-attente-de-signature");
-var fonctionnalite3 = creerBrancheDeFonctionnalite("f-IARD");
+var fonctionnalite3 = creerBrancheDeFonctionnalite("f_IARD");
 var tache3 = creerBrancheDeTache(fonctionnalite3, "t_5911-iard-table-des-id-produits");
 var tache7 = creerBrancheDeTache(fonctionnalite3, "t_5922-iard-integration-alignement");
-var fonctionnalite4 = creerBrancheDeFonctionnalite("f-DSP2");
+var fonctionnalite4 = creerBrancheDeFonctionnalite("f_DSP2");
 var tache4 = creerBrancheDeTache(fonctionnalite4, "t_auth_forte_moins_90_jours (5880)");
 var tache5 = creerBrancheDeTache(fonctionnalite4, "t_5898_auth_forte_plus_90_jours");
 var anomalie1 = creerBrancheAnomalie(integration, "a_6464-photo-accueil");
