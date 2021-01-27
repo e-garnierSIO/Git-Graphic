4ec5d9cd02050e634e31c3c55a93f47dcda8a74b
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-01-27 16:20:40 +0100

    Update sprint14.js

--- sprint14.js
+++ sprint14.js
@@ -11,7 +11,7 @@ integration.commit("start");
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_simplification_notifs");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_tu_chrome_headless");
 var fonctionnalite3 = creerBrancheDeFonctionnalite("f_LBPF");
-var tache1 = creerBrancheDeTache(fonctionnalite2, "t_5850-lbpf-en-attente-de-signature");
+var tache1 = creerBrancheDeTache(fonctionnalite3, "t_5850-lbpf-en-attente-de-signature");
 var fonctionnalite4 = creerBrancheDeFonctionnalite("f_DSP2");
 
 
