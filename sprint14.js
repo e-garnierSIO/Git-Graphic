d57d51deba86c9f49cfac7a1233934caffe32691
jesterzon <julien.esterzon@gmail.com>
2021-02-08 11:02:33 +0100

    Update sprint14.js

--- sprint14.js
+++ sprint14.js
@@ -18,7 +18,7 @@ var tache1 = creerBrancheDeTache(fonctionnalite3, "t_5850-lbpf-en-attente-de-sig
 var fonctionnalite4 = creerBrancheDeFonctionnalite("f_DSP2");
 var anomalie1 = creerBrancheAnomalie(fonctionnalite4, "a_6886_parcours_af_autre_device");
 var tache2 = creerBrancheDeTache(fonctionnalite4, "t_6890-dsp2-integration-bloc-aem");
-var anomalie2 = creerBrancheAnomalie(fonctionnalite4, "a_7108-reg-service-indisponible");
+
 var fonctionnalite5 = creerBrancheDeFonctionnalite("f_c+Stripe");
 var tache3 = creerBrancheDeTache(fonctionnalite5, "t_6613-certicode-plus-stripe");
 var fonctionnalite6 = creerBrancheDeFonctionnalite("f_gestion_cookies");
@@ -34,6 +34,7 @@ fonctionnalite3.merge(tache1);
 fonctionnalite4.merge(tache2);
 fonctionnalite4.merge(anomalie1);
 
+var anomalie2 = creerBrancheAnomalie(fonctionnalite4, "a_7108-reg-service-indisponible");
 
 // Merge branche sprint
 brancheSprint.merge(fonctionnalite3);
